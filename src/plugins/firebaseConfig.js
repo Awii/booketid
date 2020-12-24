3c682d3d2f6bf545b0c0202baca5e1a9cfe55983
awii <edgarys85@gmail.com>
2020-12-24 16:47:12 +0100

    add firebase

---
+++ src/plugins/firebaseConfig.js
@@ -0,0 +1,9 @@
+export default {
+  apiKey: "AIzaSyA-5XjvmFBEXXUZ4PPm8LQlyH9CY6KlAwk",
+  authDomain: "booketid.firebaseapp.com",
+  databaseURL: "https://booketid.firebaseio.com",
+  projectId: "booketid",
+  storageBucket: "booketid.appspot.com",
+  messagingSenderId: "484007714401",
+  appId: "1:484007714401:web:a84cf4461fcfa866ba5420"
+};
