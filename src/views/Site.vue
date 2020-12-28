b333e4c0d7c07579dd158677c032310eb96d9409
Awii <edgarys85@gmail.com>
2020-12-28 15:17:20 +0100

    add footer

--- src/views/Site.vue
+++ src/views/Site.vue
@@ -62,6 +62,92 @@
         </v-stepper>
       </v-container>
     </v-main>
+
+    <v-footer color="secondary" padless absolute app>
+      <v-container>
+        <v-row align="center" no-gutters>
+          <v-col class="d-flex justify-center pa-4">
+            <div class="d-flex flex-column white--text">
+              <h4 class="pb-2">Kontakt oss</h4>
+
+              <div class="d-flex flex-row align-center">
+                <v-icon
+                  class="mdi mdi-phone"
+                  color="white"
+                  aria-hidden="true"
+                  small
+                  dense
+                ></v-icon>
+                <div class="text-body-2 ml-2">+47 900 45 100</div>
+              </div>
+              <div class="d-flex flex-row align-center">
+                <v-icon
+                  class="mdi mdi-email"
+                  color="white"
+                  aria-hidden="true"
+                  small
+                  dense
+                ></v-icon>
+                <div class="text-body-2 ml-2">placeholder@email.com</div>
+              </div>
+            </div>
+          </v-col>
+
+          <v-col class="d-flex justify-center pa-4">
+            <div class="d-flex align-start flex-column white--text">
+              <h4 class="pb-2">Besøk oss</h4>
+
+              <div class="d-flex flex-row align-start">
+                <v-icon
+                  class="mdi mdi-map-marker"
+                  color="white"
+                  aria-hidden="true"
+                  small
+                  dense
+                ></v-icon>
+                <div class="text-body-2 ml-2 d-flex flex-column">
+                  <div>Stenersgata 1 E</div>
+                  <div>0050 Oslo</div>
+                </div>
+              </div>
+            </div>
+          </v-col>
+
+          <v-col class="d-flex justify-center pa-4">
+            <div class="d-flex align-start flex-column white--text">
+              <!--<h4 class="pb-2">Følg oss</h4>-->
+
+              <div class="d-flex flex-row">
+                <v-icon
+                  class="mdi mdi-facebook pr-1"
+                  color="white"
+                  aria-hidden="true"
+                  large
+                ></v-icon>
+                <v-icon
+                  class="mdi mdi-instagram pr-1"
+                  color="white"
+                  aria-hidden="true"
+                  large
+                ></v-icon>
+                <v-icon
+                  class="mdi mdi-twitter pr-1"
+                  color="white"
+                  aria-hidden="true"
+                  large
+                ></v-icon>
+                <v-icon
+                  class="mdi mdi-linkedin pr-1"
+                  color="white"
+                  aria-hidden="true"
+                  large
+                ></v-icon>
+              </div>
+            </div>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-footer>
   </div>
 </template>
 
