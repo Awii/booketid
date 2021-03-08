37fcbefe0033b3ed0f23fcaa0551f3505c289f22
Awii <edgarys85@gmail.com>
2021-03-08 21:10:15 +0100

    add demo user

--- src/users.js
+++ src/users.js
@@ -2,5 +2,9 @@ export default {
   Uun0qxVk8KOHCbwm4HhPnPqVBbV2: {
     fbPrefix: "demo",
     title: "Demo"
+  },
+  uvd1UyLNPNOFUOOJ6t1ptgoaMXz1: {
+    fbPrefix: "demo",
+    title: "Demo"
   }
 };
